334e5a66673d576c7b5767df550a3046d87dd46c
Zaid <9919mubashir@gmail.com>
2024-06-24 13:34:25 +0530

    Adding the comment

--- INT-1-Prime-number-identification.cpp
+++ INT-1-Prime-number-identification.cpp
@@ -1,13 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 void Isprime(int n){
+    //check the corner case
     if(n<=1){
     cout<<"Number is not Prime ";
     return ;
     }
     int limit=sqrt(n);
+    //iterate the 2 to sqrt(n)
     for(int i=2;i<=limit;i++)
     {
+        //if number is divisible then it is not prime number.
         if(n%i==0)
         {
             cout<<"Number is not Prime ";
